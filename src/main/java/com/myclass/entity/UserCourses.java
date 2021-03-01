f56fdacf3cf3157db47b813c2629efede8fd0f06
BlueSky03051993 <doanthienthanh03051993@gmail.com>
2021-03-01 13:52:50 +0700

    fix xong lỗi port 8080
    fix xong lỗi port 8080

--- src/main/java/com/myclass/entity/UserCourses.java
+++ src/main/java/com/myclass/entity/UserCourses.java
@@ -1,29 +1,37 @@
 package com.myclass.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@Table(name = "user_courses")
+@Where(clause = "active=true")
+@SQLDelete(sql = "UPDATE user_courses SET active = false WHERE id = ?")
 public class UserCourses {
 	
 	@EmbeddedId
 	UserCourseKey id;
 	
-	@ManyToOne
+	@ManyToOne(cascade = CascadeType.ALL)
 	@MapsId("userId")
 	@JoinColumn(name = "user_id")
 	User user;
 	
-	@ManyToOne
+	@ManyToOne(cascade = CascadeType.ALL)
     @MapsId("courseId")
-    @JoinColumn(name = "course_id")
+    @JoinColumn(name = "course_id",insertable = false, updatable = false)
     Course course;
 	
-	@Column(name = "role_id")
+	@Column(name = "role_id",insertable = false, updatable = false)
 	int roleId;
 
 	public UserCourseKey getId() {
