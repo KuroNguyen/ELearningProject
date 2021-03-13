6b9c0326fcdd57debe5cf478f69fc84202486fec
BlueSky03051993 <doanthienthanh03051993@gmail.com>
2021-03-13 22:52:11 +0700

    Revert "- Fix AuditorAware"
    This reverts commit b8fc72d3acc196867b2fa68336dc93d95a4f3c91.

--- src/main/java/com/myclass/core/SpringSecurityAuditorAware.java
+++ src/main/java/com/myclass/core/SpringSecurityAuditorAware.java
@@ -4,13 +4,10 @@ import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import net.bytebuddy.implementation.bind.MethodDelegationBinder.ParameterBinding.Anonymous;
 
 /**
  * Author: Nguyen Chanh Truc Created: Jan 31, 2021
@@ -20,10 +17,12 @@ public class SpringSecurityAuditorAware implements AuditorAware<String> {
 	@Override
 	public Optional<String> getCurrentAuditor() {
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication instanceof AnonymousAuthenticationToken)
-			return Optional.of("Anonymous");
-
+//		SecurityContextHolder contextHolder = (SecurityContextHolder) SecurityContextHolder.getContext();
+//		SecurityContext securityContext = SecurityContextHolder.getContext();
+//		if (!securityContext.getAuthentication().isAuthenticated()) {
+//			return Optional.ofNullable("Anonymous").filter(s -> !s.isEmpty());
+//		}
+//
 		return Optional.ofNullable(SecurityContextHolder.getContext()).map(SecurityContext::getAuthentication)
 				.filter(Authentication::isAuthenticated).map(Authentication::getPrincipal).map(UserDetails.class::cast)
 				.map(UserDetails::getUsername);
